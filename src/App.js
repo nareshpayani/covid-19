4f2b50652a27296e27875c70d38c8f474761cdd5
Naresh Payani <nareshpayani@Nareshs-iMac.local>
2020-09-21 21:27:50 +0530

    Cleanup Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1>COVID-19 TRACKER</h1>
+
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="">
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            <MenuItem value="">India</MenuItem>
+            <MenuItem value="">US</MenuItem>
+            <MenuItem value="">Brazil</MenuItem>
+            <MenuItem value="">America</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/* Headder */}
+
+        {/* Info Box */}
+
+        {/* Table */}
+
+        {/* Chart */}
+
+        {/* Map */}
     </div>
   );
 }
